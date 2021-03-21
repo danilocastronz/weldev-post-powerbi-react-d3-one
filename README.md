25c9a5273877af11ac46bf9fb6ca7d7fea47b836
Danilo Castro <danilo.castro.nz@gmail.com>
2021-03-22 11:26:35 +1300

    Created React application.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome, developer
+
+This code is from the post **Building Power BI custom visuals with React and D3.js | Pt. One** from [WelcomeDeveloper.com](https://www.welcomedeveloper.com/)
---
+++ welDevReactVisual/src/visual.ts
@@ -0,0 +1,118 @@
+/*
+ *  Power BI Visual CLI
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+"use strict";
+import "core-js/stable";
+import "../style/visual.less";
+import powerbi from "powerbi-visuals-api";
+import IVisual = powerbi.extensibility.IVisual;
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
+
+import * as d3 from "d3";
+type Selection<T extends d3.BaseType> = d3.Selection<T, any, any, any>;
+
+import DataView = powerbi.DataView;
+
+import { VisualSettings } from "./settings";
+
+export class Visual implements IVisual {
+  private host: IVisualHost;
+  private svg: Selection<SVGElement>;
+  private container: Selection<SVGElement>;
+  private circle: Selection<SVGElement>;
+  private textValue: Selection<SVGElement>;
+  private textLabel: Selection<SVGElement>;
+
+  private visualSettings: VisualSettings;
+
+  constructor(options: VisualConstructorOptions) {
+    this.svg = d3
+      .select(options.element)
+      .append("svg")
+      .classed("circleCard", true);
+    this.container = this.svg.append("g").classed("container", true);
+    this.circle = this.container.append("circle").classed("circle", true);
+    this.textValue = this.container.append("text").classed("textValue", true);
+    this.textLabel = this.container.append("text").classed("textLabel", true);
+  }
+
+  public enumerateObjectInstances(
+    options: EnumerateVisualObjectInstancesOptions
+  ): VisualObjectInstanceEnumeration {
+    const settings: VisualSettings =
+      this.visualSettings || <VisualSettings>VisualSettings.getDefault();
+    return VisualSettings.enumerateObjectInstances(settings, options);
+  }
+
+  public update(options: VisualUpdateOptions) {
+    let dataView: DataView = options.dataViews[0];
+
+    let width: number = options.viewport.width;
+    let height: number = options.viewport.height;
+    this.svg.attr("width", width);
+    this.svg.attr("height", height);
+    let radius: number = Math.min(width, height) / 2.2;
+
+    this.visualSettings = VisualSettings.parse<VisualSettings>(dataView);
+
+    this.visualSettings.circle.circleThickness = Math.max(
+      0,
+      this.visualSettings.circle.circleThickness
+    );
+    this.visualSettings.circle.circleThickness = Math.min(
+      10,
+      this.visualSettings.circle.circleThickness
+    );
+
+    this.circle
+      .style("fill", this.visualSettings.circle.circleColor)
+      .style("fill-opacity", 0.5)
+      .style("stroke", "pink")
+      .style("stroke-width", this.visualSettings.circle.circleThickness)
+      .attr("r", radius)
+      .attr("cx", width / 2)
+      .attr("cy", height / 2);
+    let fontSizeValue: number = Math.min(width, height) / 5;
+    this.textValue
+      .text(<string>dataView.single.value)
+      .attr("x", "50%")
+      .attr("y", "50%")
+      .attr("dy", "0.35em")
+      .attr("text-anchor", "middle")
+      .style("font-size", fontSizeValue + "px");
+    let fontSizeLabel: number = fontSizeValue / 4;
+    this.textLabel
+      .text(dataView.metadata.columns[0].displayName)
+      .attr("x", "50%")
+      .attr("y", height / 2)
+      .attr("dy", fontSizeValue / 1.2)
+      .attr("text-anchor", "middle")
+      .style("font-size", fontSizeLabel + "px");
+  }
+}
